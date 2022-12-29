2575be6c49fc4535eb97ecb55021f2b3d3bf6fe1
Bence <olonor.gest01@gmail.com>
2022-12-29 16:33:26 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const input = document.querySelector(".input");
+const clear = document.querySelector(".button--clear");
+const add = document.querySelector(".button--add");
+const list = document.querySelector(".list");
